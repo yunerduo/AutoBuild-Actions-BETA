1a9766fd68128280cccbf05210f44eab6268aea3
蓝杰-X15笔记本电脑 <lanjie1990@vip.qq.com>
2026-08-13 19:30:25 +0800

    fix: restrict zzz_Default_Version to single line to avoid multi-line GITHUB_ENV format error

--- Scripts/AutoBuild_Function.sh
+++ Scripts/AutoBuild_Function.sh
@@ -23,7 +23,7 @@ Firmware_Diy_Start() {
 	case "${OP_AUTHOR}/${OP_REPO}" in
 	coolsnowwolf/lede)
 		Version_File=package/lean/default-settings/files/zzz-default-settings
-		zzz_Default_Version="$(egrep -o "R[0-9]+\.[0-9]+\.[0-9]+" ${Version_File})"
+		zzz_Default_Version="$(egrep -o "R[0-9]+\.[0-9]+\.[0-9]+" ${Version_File} | awk 'NR==1')"
 		OP_VERSION="${zzz_Default_Version}-${Compile_Date}"
 	;;
 	immortalwrt/immortalwrt | padavanonly/immortalwrtARM | hanwckf/immortalwrt-mt798x)
